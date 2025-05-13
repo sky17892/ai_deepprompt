d461a548f234d5e17ece93790384eeef75dfc373
yj <sky17893@gmail.com>
2025-05-13 15:29:29 +0900

    update12345 commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# .env 파일을 생성하여 HF_TOKEN 환경 변수 값을 기록
+# .env 파일을 생성
 echo "HF_TOKEN=$HF_TOKEN" > .env
+
+# Vercel 로그인 및 배포 (환경변수 필요 시 --env 사용)
+npx vercel --token=$VERCEL_TOKEN --prod
